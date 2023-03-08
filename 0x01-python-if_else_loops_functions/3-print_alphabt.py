5f25ed36b7e0843c021ff3a56d3fa77f2aa07bcd
Jesse <esaujesse@yahoo.com>
2023-03-08 08:08:45 +0100

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print('{0} = {1}'.format(num, hex(num)))
+for i in range(97, 123):
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print(chr(i).format(i), end='')
 
