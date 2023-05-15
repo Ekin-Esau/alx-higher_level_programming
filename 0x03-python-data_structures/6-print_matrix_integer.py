02c57ad0939b518c40437a4f8db7ce256d7b5b21
Jesse <esaujesse@yahoo.com>
2023-05-15 17:32:53 +0100

    Fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,10 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             print("{:d}".format(j), end=" ")
-        print(" ")
-
-print_matrix_integer( [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-    ])
+        print()
