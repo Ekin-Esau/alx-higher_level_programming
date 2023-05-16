8cd0acab6848f10802e81b7e4a68255f3c7a80fe
Jesse <esaujesse@yahoo.com>
2023-05-16 15:12:25 +0100

    Another fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    message_1 = "{} is divisible by 2"
-    message_2 = "{} is not divisible by 2"
     for i in my_list:
         if i % 2 == 0:
-            return message_1
+            print("{} is divisible by 2".format(i))
         else:
-            return message_2
+            print("{} is not divisible by 2".format(i))
