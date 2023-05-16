45d0ce64668a5128e354548bd411f471a90346c7
Jesse <esaujesse@yahoo.com>
2023-05-16 13:36:05 +0100

    Another fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
     if len(sentence) == 0:
         first_char = None
+    else:
+        first_char = sentence[0]
     return length, first_char
+
+
+
