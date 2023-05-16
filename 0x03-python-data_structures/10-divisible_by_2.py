5e5c86603ea79ecbaf1700f933553b20443ec85c
Jesse <esaujesse@yahoo.com>
2023-05-16 14:59:17 +0100

    All multiples of 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    message_1 = f"{} is divisible by 2"
+    message_2 = f"{} is not divisible by 2"
+    for i in my_list:
+        if i % 2 == 0:
+            return message_1
+        else:
+            return message_2
+
