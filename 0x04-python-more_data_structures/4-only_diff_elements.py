1d45da55a1f7acbea05ddc73d31f68b9a2f47a0e
Jesse <esaujesse@yahoo.com>
2023-05-17 15:57:57 +0100

    Bug fixed

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set_3 = set()
-    for i, j in set_1, set_2:
-        if i in set_1 and i not in set_2:
-            set_3.add(i)
-        if j in set_2 and j not in set_1:
-            set_3.add(j)
-    return set_3
+    return set_1.symmetric_difference(set_2)
