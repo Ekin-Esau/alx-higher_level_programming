e105aa7a746dbc294ae9c7aeda04b1305402a0e7
Jesse <esaujesse@yahoo.com>
2023-05-17 14:15:32 +0100

    Replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def rearch_replace(my_list, search, replace):
+    if len(my_list) == 0:
+        return my_list
+    my_list = [[search = replace for i in j] for j in my_listi]
+    return my_list
