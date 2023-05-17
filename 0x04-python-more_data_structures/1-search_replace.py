b74a6d626d0db8173bfd392eff559f4e2d65a44a
Jesse <esaujesse@yahoo.com>
2023-05-17 14:25:16 +0100

    Fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,7 @@ def rearch_replace(my_list, search, replace):
     if len(my_list) == 0:
         return my_list
     for i in my_list:
-        i.replace(search, replace)
+        if i = search:
+            i = replace
     return my_list
 
