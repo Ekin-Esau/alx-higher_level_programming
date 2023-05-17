50b44a0774a34bda548f0edf45fbd5608272d1bb
Jesse <esaujesse@yahoo.com>
2023-05-17 14:54:14 +0100

    Fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
     for i in my_list:
         if i in my_list and i not in new_list:
             new_list.append(i)
-        return sum(new_list)
+    return sum(new_list)
 
 
 
