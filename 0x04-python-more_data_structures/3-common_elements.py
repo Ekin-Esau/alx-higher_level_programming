44eeb4ec513a072abeebf8d06356e6ea6abca4c5
Jesse <esaujesse@yahoo.com>
2023-05-17 15:35:37 +0100

    Fix

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_3 = {}
-    for i in set_1, set_2:
-        if i in set_1 and i in set_2:
-            set_3.append(i)
+    set_3 = set()
+    for i in set_1:
+        if i in set_2:
+            set_3.add(i)
     return set_3
